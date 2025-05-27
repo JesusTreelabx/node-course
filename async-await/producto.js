017207a9ef45235fa6d05917d93d153b415447b0
Jesus Capetillo <jesscvdevops@gmail.com>
2025-05-27 09:03:39 -0600

    ADD: new files pt4

---
+++ async-await/producto.js
@@ -0,0 +1,35 @@
+function ordenarProducto(producto) {
+    return new Promise((resolve, reject) => {
+        console.log(`ordenando: ${producto} de YisusCapetillo`);
+        setTimeout(() => {
+            if (producto === 'bebida') {
+                resolve ('Ordenando una bebida berry pearls de Starbucks');
+            } else {
+                reject('¡Este producto no esta disponible!');
+            }
+        }, 4000);
+    }); 
+}
+
+function procesarPedido(respuesta) {
+    return new Promise(resolve => {
+        console.log('procesando respuesta...');
+        console.log(`la respuesta fue: "${respuesta}"`);
+        setTimeout(() => {
+            resolve('Gracias por tu compra. Disfruta tu bebida de Starbucks');
+        }, 4000 );
+    });
+}
+
+ordenarProducto('bebida')
+    .then(respuesta => {
+        console.log('Respuesta, recibida');
+        console.log(respuesta);
+        return procesarPedido(respuesta);
+    })
+    .then(respuestaProcesada => {
+        console.log(respuestaProcesada);
+    })
+    .catch(error => {
+        console.log(error);
+    });
---
+++ promesas-js/pedido.js
@@ -0,0 +1,29 @@
+const statusPedido = () => {
+    return Math.random() < 0.8;
+};
+
+//for (let i =0; i < 10; i++) {
+//    console.log(statusPedido());
+//};
+
+const miPedidoDePizza = new Promise ((resolve, reject) => {
+    setTimeout(() => {
+        if (statusPedido()) {
+            resolve('¡Pedido exitoso! Su Pizza está en camino.');
+        } else {
+            reject ('¡Ocurrio un error porfavor! intente nuevamente')
+        }
+    }, 3000);
+});
+
+const manejarPedido = (mensajeDeConfirmacion) =>  {
+    console.log(mensajeeConfirmacion);
+};
+
+const rechazarPedido = (mensajeDeError) => {
+    console.log(mensajeDeError);
+}; 
+
+miPedidoDePizza
+    .then(manejarPedido) 
+    .catch(rechazarPedido);
